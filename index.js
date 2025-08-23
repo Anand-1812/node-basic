a61d306668137d6acb41990274d77a4b2c5de52a
Anand <anand.work2005@gmail.com>
2025-08-23 21:15:11 +0530

    raw file serving with nodeJs

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-import express from 'express'
+import http from 'node:http';
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
-const app = express();
-const PORT = 1234
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const PORT = 1234;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = http.createServer((req, res) => {
+  let filePath = "";
+
+  if (req.url === '/' || req.url === '/index.html') {
+    filePath = path.join(__dirname, 'index.html');
+  } else if (req.url === '/about') {
+    filePath = path.join(__dirname, 'about.html');
+  } else if (req.url === '/contact') {
+    filePath = path.join(__dirname, 'contact-me.html');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+    return;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 Server Error');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
